c97579d88468b21ddf7a925a50db024739da52fc
Aman sharma <87026675+Aman300@users.noreply.github.com>
2024-08-20 11:20:58 +0530

    update-ui

--- Client/src/components/Sidebar.jsx
+++ Client/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 // Sidebar.js
 import { useState, } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-
+import { BookOpenCheck, Contact, LayoutDashboard, ListCheck, ListTodo, MonitorCheckIcon, MonitorCog, NotebookTabs, Presentation, UserPlus } from 'lucide-react';
 
 
 
@@ -31,43 +31,43 @@ const Sidebar = () => {
         {
           name: "Dashboard",
           path: "/",
-          svgLogo: "M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z"
+          svgLogo: <LayoutDashboard/>
         },
         {
           name: "Add Department",
           path: "/add/department",
-          svgLogo: "M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
+          svgLogo: <ListTodo/>
         },
         {
           name: "Add Employee",
           path: "/add/employee",
-          svgLogo: "M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3"
+          svgLogo: <UserPlus/>
         },
         {
           name: "Employee List",
           path: "/employee/list",
-          svgLogo: "M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 0 1-.657.643 48.39 48.39 0 0 1-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 0 1-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 0 0-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 0 1-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 0 0 .657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 0 1-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 0 0 5.427-.63 48.05 48.05 0 0 0 .582-4.717.532.532 0 0 0-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 0 0 .658-.663 48.422 48.422 0 0 0-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 0 1-.61-.58v0Z"
+          svgLogo: <Contact/>
         },
 
         {
           name: "Today Attendance",
           path: "/today/attendance",
-          svgLogo: "M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"
+          svgLogo: <ListCheck/>
         },
         {
           name: "Attendance List",
           path: "/attendance/list",
-          svgLogo: "M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+          svgLogo: <NotebookTabs/>
         },
         {
           name: "Notice",
           path: "/notice",
-          svgLogo: "M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z"
+          svgLogo: <Presentation/>
         },
         {
           name: "Settings",
           path: "/settings",
-          svgLogo: "M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z"
+          svgLogo: <MonitorCog/>
         },
        
       ]
@@ -144,7 +144,7 @@ const Sidebar = () => {
                           onClick={() => handleTabClick(index)}
                         >
                           <span className="inline-flex justify-center items-center ml-4">
-                            <svg
+                            {/* <svg
                               className="w-5 h-5"
                               fill="none"
                               stroke="currentColor"
@@ -157,7 +157,8 @@ const Sidebar = () => {
                                 strokeWidth={2}
                                 d={link.svgLogo}
                               />
-                            </svg>
+                            </svg> */}
+                            {link.svgLogo}
                           </span>
                           <span className="ml-2 text-sm tracking-wide truncate">
                             {link.name}
@@ -216,11 +217,11 @@ const Sidebar = () => {
                       <li key={index}>
                         <Link
                           to={link.path}
-                          className={`relative flex flex-row items-center h-11 focus:outline-none rounded-xl hover:text-white hover:bg-[#f5f7f9] hover:bg-opacity-5 bg-opacity-5 border-transparent pr-6 ${activeTab === index ? 'bg-[#f5f7f9] text-white' : 'text-gray-600'}`}
+                          className={`relative flex flex-row items-center h-11 focus:outline-none rounded-xl  hover:bg-[#f5f7f9] hover:bg-opacity-5 bg-opacity-5 border-transparent pr-6 ${activeTab === index ? 'bg-[#f5f7f9] text-white' : 'text-gray-600'}`}
                           onClick={() => handleTabClick(index)}
                         >
-                          <span className="inline-flex justify-center items-center ml-4">
-                            <svg
+                          <span className="w-5 h-5 inline-flex justify-center items-center ml-4">
+                            {/* <svg
                               className="w-5 h-5"
                               fill="none"
                               stroke="currentColor"
@@ -233,10 +234,11 @@ const Sidebar = () => {
                                 strokeWidth={2}
                                 d={link.svgLogo}
                               />
-                            </svg>
+                            </svg> */}
+                             {link.svgLogo}
                           </span>
                           
-                          <span className={`ml-2 font-semibold hover:text-white  ${activeTab === index ? 'text-white' : 'text-zinc-500'}`}>
+                          <span className={`ml-2 text-sm font-semibold  ${activeTab === index ? 'text-white' : 'text-zinc-500'}`}>
                             {link.name}
                           </span>
                         </Link>
